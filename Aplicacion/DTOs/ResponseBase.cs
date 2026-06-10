d765c9c8df0e977b1a4de8ed7e1046549f42012a
alex16leiva <alex16leiva@gmail.com>
2026-06-09 18:30:19 -0600

    Eliminando el manejo de los mensajes de validacion del responseBase, se manejaran con elpatron response

--- Aplicacion/DTOs/ResponseBase.cs
+++ Aplicacion/DTOs/ResponseBase.cs
@@ -4,30 +4,6 @@ namespace Aplicacion.DTOs
 {
     public abstract class ResponseBase
     {
-        public string? Message { get; set; }
-        public string? ValidationErrorMessage { get; set; }
-        public string? SuccessMessage { get; set; }
         public DateTime? FechaTransaccion { get; set; }
-
-        public bool HasValidationMessage()
-        {
-            return Message.HasValue();
-        }
-
-        public bool HasValidationErrorMessage()
-        {
-            return !string.IsNullOrWhiteSpace(ValidationErrorMessage);
-        }
-
-        public void AppendValidationErrorMessage(string message)
-        {
-            if (HasValidationErrorMessage())
-            {
-                ValidationErrorMessage = $"{ValidationErrorMessage}, {message}";
-                return;
-            }
-
-            ValidationErrorMessage = message;
-        }
     }
 }
